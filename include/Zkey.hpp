ae27e4bc436933bf6a4d5438e1707f5faa30cbe2
Dionisis Pettas <dions.pettas@gmail.com>
2023-04-21 23:25:49 +0300

    Created the Zkey for Morton Interleaving

---
+++ include/Zkey.hpp
@@ -0,0 +1,16 @@
+#ifndef ZKEY_HPP
+#define ZKEY_HPP
+
+#include <stdint.h>
+
+// Define the Zkey type
+using Zkey_t = uint64_t;
+
+namespace zkey
+{
+    Zkey_t encode(              uint32_t x, uint32_t y);
+    void   decode( Zkey_t zkey, uint32_t x, uint32_t y);
+
+}
+
+#endif // ZKEY_HPPP
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Zkey.hpp"
+
+int main(int argc, char** argv) 
+{
+
+std::cout << zkey::encode(1.0, 2.0) << std::endl;
+    std::cout << "Hello, world!\n";
+}
